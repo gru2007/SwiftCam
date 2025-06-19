3329259e01612c7bdef548725e9ddf3dd9c3ac7c
Gru <ruslru376@gmail.com>
2025-06-19 18:07:47 +1000

    Initial Commit

---
+++ SwiftCamTests/SwiftCamTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftCamTests.swift
+//  SwiftCamTests
+//
+//  Created by Руслан Артемьев on 19.06.2025.
+//
+
+import Testing
+
+struct SwiftCamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
